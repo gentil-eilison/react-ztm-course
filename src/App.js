911551ec4efced46281a26f357a345e44a11158d
Gentil Ályson <fernandes.gentilalysonjac@gmail.com>
2023-02-22 13:39:09 -0300

    finished 'Functional Components' section

--- src/App.js
+++ src/App.js
@@ -1,57 +1,90 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super()
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    }
-  }
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => setMonsters(users))
+  }, [])
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then(response => {
-      return response.json()
-    }).then(users => {
-      this.setState(() => {
-        return { monsters: users }
-      })
-    })
-  }
-  
-  /**
-   * We need to move that anonymous function here,
-   * because it doesn't get stored in memory, 
-   * causing React to redefine them every ttime the
-   * component re-renders.
-   */
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase()
-    this.setState({ searchField })
-  }
-
-  render() {
-    // This makes the code more human readable
-    const { monsters, searchField } = this.state
-    const { onSearchChange } = this
-
-    const filteredMonsters = monsters.filter(monster => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster => {
       return monster.name.toLowerCase().includes(searchField)
     })
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox placeholder="search monsters" className="search-box" onChangeHandler={onSearchChange}/>
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    );
+    setFilteredMonsters(newFilteredMonsters)
+  }, [monsters, searchField])
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase()
+    setSearchField(searchFieldString)
   }
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox placeholder="search monsters" className="search-box" onChangeHandler={onSearchChange}/>
+      <CardList monsters={filteredMonsters}/>
+    </div>
+  )
 }
+
+// class App extends Component {
+//   constructor() {
+//     super()
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users").then(response => {
+//       return response.json()
+//     }).then(users => {
+//       this.setState(() => {
+//         return { monsters: users }
+//       })
+//     })
+//   }
+  
+//   /**
+//    * We need to move that anonymous function here,
+//    * because it doesn't get stored in memory, 
+//    * causing React to redefine them every ttime the
+//    * component re-renders.
+//    */
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLowerCase()
+//     this.setState({ searchField })
+//   }
+
+//   render() {
+//     // This makes the code more human readable
+//     const { monsters, searchField } = this.state
+//     const { onSearchChange } = this
+
+//     const filteredMonsters = monsters.filter(monster => {
+//       return monster.name.toLowerCase().includes(searchField)
+//     })
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox placeholder="search monsters" className="search-box" onChangeHandler={onSearchChange}/>
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,12 @@
-import { Component } from 'react';
-
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-    render() {
-        return (
-            <input 
-            className={`search-box ${this.props.className}`}
-            onChange={this.props.onChangeHandler}
-            placeholder={this.props.placeholder}
-            type="search" 
-            />
-        )
-    }
-}
+const SearchBox = ({ className, onChangeHandler, placeholder }) => (
+    <input 
+    className={`search-box ${className}`}
+    onChange={onChangeHandler}
+    placeholder={placeholder}
+    type="search" 
+    />
+)
 
 export default SearchBox
